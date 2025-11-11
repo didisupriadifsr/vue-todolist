fdfcdbae563941e94f4c9d5e95ca0730636b35e3
Didi Supriadi <didi.supriadi@bssn.go.id>
2025-11-11 17:07:12 +0700

    rfr: from options to compotition

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -8,19 +8,23 @@
   </label>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 // options api
-import { defineComponent } from 'vue'
+// import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'TaskComponent',
-  props: {
-    isChecked: {
-      type: Boolean,
-      default: false,
-    },
-  },
-})
+// export default defineComponent({
+//   name: 'TaskComponent',
+//   props: {
+//     isChecked: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+// })
+
+defineProps<{
+  isChecked: boolean | false
+}>()
 </script>
 
 <style scoped>
