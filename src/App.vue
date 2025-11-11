5c8d83ef7be5d30f2e88840a523668c9ddc7f29d
Didi Supriadi <didi.supriadi@bssn.go.id>
2025-11-11 16:16:51 +0700

    refactor: enhance App.vue structure and styling; add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
+<!-- Javascript -->
 <script setup lang="ts"></script>
 
+<!-- HTML -->
 <template>
-  <h1>You DIDI</h1>
+  <h1>👌You DIDI</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
 
-<style scoped></style>
+<!-- CSS -->
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:
+    poppins,
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+}
+
+body {
+  background-color: #59f9f9;
+  color: #f9f6f6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+#app {
+  min-height: 100vh;
+  text-align: center;
+  background: linear-gradient(135deg, #afbae8 0%, #667eea 50%);
+}
+</style>
